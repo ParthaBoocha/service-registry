7a2bce5c0904669d91bdef7c75c3f45ceaf05f9c
Partha Boocha <partha@boocha>
2017-07-03 00:41:01 -0400

    using literal css, need to get webpack for css working

--- server/web/config-list-item.jsx
+++ server/web/config-list-item.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ServiceStatusIndicator from './service-status-indicator';
-import style from './styles/main.css';
+import styles from './styles/main.css';
 
 export default class ConfigListItem extends React.PureComponent {
   render() {
     return (
-      <div className={style.service}>
-        <h3>{this.props.config.service}</h3>
-        <div>{this.props.config.url}</div>
-        <div>{this.props.config.port}</div>
-        <ServiceStatusIndicator status={this.props.status} />
+      <div className={'card'}>
+        <div className={'content'}>
+          <h3>{this.props.config.service}</h3>
+          <p>{this.props.config.url}</p>
+          <p>{this.props.config.port}</p>
+          <ServiceStatusIndicator status={this.props.status} />
+        </div>
       </div>
     );
   }
