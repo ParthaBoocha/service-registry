d82096146f1f5b131bfe0fdb0f8f4007cf5f6a43
Partha Boocha <partha@boocha>
2017-07-06 08:03:20 -0400

    added MSpec test lib. Having test runner issues

---
+++ nuget/service-registry-tests/HelloWorldSpec.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using service_registry;
+using Machine.Specifications;
+
+namespace service_registry_tests
+{
+    [Subject(typeof(HelloWorld))]
+    public class when_saying_hello
+    {
+        It should_say_hello_world = () => { new HelloWorld().SayHello(); };
+    }
+}
