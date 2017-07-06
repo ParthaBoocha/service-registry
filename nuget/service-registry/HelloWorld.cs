37115ed5dcde3ea928988a30b8b0a0f4a5ae1d82
Partha Boocha <partha@boocha>
2017-07-05 22:54:27 -0400

    dot-net core project bootstrap

---
+++ nuget/service-registry/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace service_registry
+{
+    public class HelloWorld
+    {
+        public string SayHello()
+        {
+            return "Hello World";
+        }
+    }
+}
