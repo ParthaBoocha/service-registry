e6af95bcc3b574df9e9987323973869ed0a23a61
Partha Boocha <partha@boocha>
2017-07-07 08:03:29 -0400

    finally after 2 days, tests are running but with netcoreapp1.1

--- nuget/service-registry-tests/HelloWorldSpec.cs
+++ nuget/service-registry-tests/HelloWorldSpec.cs
@@ -7,6 +7,12 @@ namespace service_registry_tests
     [Subject(typeof(HelloWorld))]
     public class when_saying_hello
     {
-        It should_say_hello_world = () => { new HelloWorld().SayHello(); };
+        It should_say_hello_world = () => { new HelloWorld().SayHello().ShouldEqual("Hello World"); };
+    }
+
+    [Subject(typeof(HelloWorld))]
+    public class failing_spec
+    {
+        It should_say_hello_universe = () => { new HelloWorld().SayHello().ShouldEqual("Hello Universe"); };
     }
 }
