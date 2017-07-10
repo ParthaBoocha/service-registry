6056dc7aee64a066f15f446c24e33b960c9b3c02
partha.boocha@gmail.com <partha.boocha@gmail.com>
2017-07-10 16:59:26 -0400

    added tests for get all

--- nuget/service-registry-tests/When_getting_all_configs.cs
+++ nuget/service-registry-tests/When_getting_all_configs.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Machine.Specifications;
+using NSubstitute;
 using service_registry;
 
 namespace service_registry_tests
@@ -9,17 +10,21 @@ namespace service_registry_tests
     {
         Establish establish = () =>
         {
+            _cache = Substitute.For<ILocalCache>();
             Subject = new ConfigurationService(new MockMessageHandler(
                 "[{\"service\": \"xyz\", \"url\": \"xyzhost\", \"port\": \"1234\"},"
                 + "{\"service\": \"abc\", \"url\": \"abchost\", \"port\": \"567\"}]"
-            ));
+            ),
+            _cache);
         };
 
         Because of = () => _configs = Subject.GetAll(@"http://url").Await();
 
         It should_return_an_empty_config = () => _configs.Count.ShouldEqual(2);
+        It should_save_to_local_cache = () => _cache.Received().Save(Arg.Any<Configuration[]>());
 
         private static ConfigurationService Subject;
         private static List<Configuration> _configs;
+        private static ILocalCache _cache;
     }
 }
--- nuget/service-registry-tests/When_no_configs_exist_and_getting_all.cs
+++ nuget/service-registry-tests/When_no_configs_exist_and_getting_all.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Machine.Specifications;
+using NSubstitute;
 using service_registry;
 
 namespace service_registry_tests
@@ -9,14 +10,17 @@ namespace service_registry_tests
     {
         Establish establish = () =>
         {
-            Subject = new ConfigurationService(new MockMessageHandler("[]"));
+            _cache = Substitute.For<ILocalCache>();
+            Subject = new ConfigurationService(new MockMessageHandler("[]"), _cache);
         };
 
         Because of = () => _configs = Subject.GetAll(@"http://url").Await();
 
         It should_return_an_empty_config = () => _configs.Count.ShouldEqual(0);
+        It should_not_save_to_local_cache = () => _cache.DidNotReceive().Save(Arg.Any<Configuration[]>());
 
         private static ConfigurationService Subject;
         private static List<Configuration> _configs;
+        private static ILocalCache _cache;
     }
 }
