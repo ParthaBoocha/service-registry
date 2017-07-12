1f641611669f6a19bf87d73305ef83da136ad79f
partha.boocha@gmail.com <partha.boocha@gmail.com>
2017-07-12 14:27:35 -0400

    adding unit test for when getting from both registry and local cache fails

--- nuget/service-registry-tests/When_reading_from_local_cache_and_it_errors.cs
+++ nuget/service-registry-tests/When_reading_from_local_cache_and_it_errors.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using Machine.Specifications;
 using NSubstitute;
 using service_registry;
@@ -12,11 +13,12 @@ namespace service_registry_tests
         {
             _cache = Substitute.For<ILocalCache>();
             _cache.When(x => x.Read()).Do(x => { throw new Exception("some error"); });
-            Subject = new ConfigurationService(new MockMessageHandler(""), _cache);
+            Subject = new ConfigurationService(new MockMessageHandler("", HttpStatusCode.InternalServerError), _cache);
         };
 
         Because of = async () => _config = await Subject.GetConfiguration("http://someurl", "xyz");
 
+        It should_attempt_to_read_from_local_cache = () => _cache.Received().Read();
         It should_return_an_empty_config = () => _config.Service.ShouldBeNull();
 
         private static ILocalCache _cache;
