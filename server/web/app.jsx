90539563b6ded78fa9462a0c5e7b6c4b2eaf623a
Partha Boocha <partha@boocha>
2017-07-13 18:34:13 -0400

    introducing react router and related style changes

--- server/web/app.jsx
+++ server/web/app.jsx
@@ -5,16 +5,36 @@ require('babel-polyfill');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
 import ServiceRegistry from './service-registry';
-import { AppBar } from 'react-toolbox/lib/app_bar';
+import AppBar from 'react-toolbox/lib/app_bar';
+import Navigation from 'react-toolbox/lib/navigation';
+import styles from './styles/main';
+
+class AppSettings extends React.PureComponent {
+  render() {
+    return (
+      <div className={styles.mainContent}>
+    App Settings Page
+      </div>);
+  }
+};
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <AppBar title='Service Registry' fixed />
-        <ServiceRegistry />
-      </div>
+      <HashRouter>
+        <div>
+          <AppBar title='Service Registry' fixed>
+            <Navigation type='horizontal'>
+              <Link to='/serviceRegistry'>Service Registry</Link>
+              <Link to='/appSettings'>App Settings</Link>
+            </Navigation>
+          </AppBar>
+          <Route path='/serviceRegistry' component={ServiceRegistry} />
+          <Route path='/appSettings' component={AppSettings} />
+        </div>
+      </HashRouter>
     );
   }
 };
